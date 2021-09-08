2d3cc24b023704fff8c4f3f1dee76eff5f7008d3
Cesar <cesdia883@gmail.com>
2021-09-07 22:46:08 -0400

    lint clean up

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -10,14 +10,19 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center w-full my-3">
         {/* <div className="bg-sky rounded-full h-96 w-full opacity-40 absolute z-0 bottom-3" /> */}
         <div className="relative w-full h-40 sm:h-52 mb-3 ">
-          <Image src="/Muhammad-Ali.jpg" layout="fill" objectFit="contain" />
+          <Image
+            src="/Muhammad-Ali.jpg"
+            layout="fill"
+            objectFit="contain"
+            alt="Muhammad Ali knocking down opponent"
+          />
         </div>
         <button className="bg-darkBlue text-light p-5 font-code rounded-md">
           <h1>Page Under Production</h1>
-          <p className="mt-2">
+          <p className="mt-2 font-code">
             In the mean time. Check out the
             <Link href="/blog">
-              <strong className="text-sky"> blog</strong>
+              <a className="font-bold text-sky"> blog</a>
             </Link>
           </p>
         </button>
