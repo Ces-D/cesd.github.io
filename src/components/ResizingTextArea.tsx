764de41e9c10bd968bc3cf39875f5564d50edd28
Ces-D <cesdia883@gmail.com>
2022-08-21 21:14:00 -0400

    text area looks and works better

--- src/components/ResizingTextArea.tsx
+++ src/components/ResizingTextArea.tsx
@@ -1,7 +1,11 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
+import type { ChangeEventHandler, KeyboardEventHandler } from "react"
 
 const ResizingTextArea = ({ disabled, value }: Partial<Pick<HTMLTextAreaElement, "disabled" | "value">>) => {
   const textAreaRef = useRef<HTMLTextAreaElement>(null)
+  const [textValue, setTextValue] = useState(value ?? '')
+  const MIN_ROWS = 1
+  const MAX_ROWS = 5
 
   const inputWidth = (el: HTMLTextAreaElement): string => {
     const text = el.value || el.placeholder
@@ -15,9 +19,7 @@ const ResizingTextArea = ({ disabled, value }: Partial<Pick<HTMLTextAreaElement,
     return textWidth + "px"
   }
 
-  const resizeElement = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const minRows = 1
-    const maxRows = 5
+  const onChangeResizeElementHandler: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
 
     // store initial inline overflow property value in a variable for later reverting to original condition
     const initialInlineOverflowY = e.target.style.overflowY;
@@ -27,11 +29,10 @@ const ResizingTextArea = ({ disabled, value }: Partial<Pick<HTMLTextAreaElement,
 
     const totalWidth = inputWidth(e.target);
     e.target.style.width = totalWidth;
-
-    let rows = minRows;
+    let rows = MIN_ROWS
     e.target.setAttribute("rows", rows.toString());
 
-    while (rows <= maxRows && e.target.scrollHeight !== e.target.clientHeight) {
+    while (rows <= MAX_ROWS && e.target.scrollHeight !== e.target.clientHeight) {
       e.target.setAttribute("rows", rows.toString());
       rows++;
     }
@@ -42,12 +43,40 @@ const ResizingTextArea = ({ disabled, value }: Partial<Pick<HTMLTextAreaElement,
     } else {
       e.target.style.removeProperty("overflow-y");
     }
+  }
 
+  const onKeyDownHandler: KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
+    if (e.key === 'c' && e.ctrlKey) {
+      e.preventDefault()
+      setTextValue('')
+      console.log('CTRL C PRESSED')
+    }
+    if (e.key === 'Tab') {
+      e.preventDefault()
+      console.log('TAB PRESSED')
+    }
+    if (e.key === 'Enter' || e.code === '13') {
+      e.preventDefault()
+      console.log('ENTER PRESSED')
+    }
+    if (e.key === 'ArrowUp') {
+      e.preventDefault()
+      console.log('ARROW UP PRESSED')
+    }
+    if (e.key === 'ArrowDown') {
+      e.preventDefault()
+      console.log('ARROW DOWN PRESSED')
+    }
   }
 
   return (
-    <textarea id="prompt" value={value} disabled={disabled} ref={textAreaRef} autoFocus autoComplete="off" autoCorrect="off"
-      autoCapitalize="off" maxLength={150} onInput={resizeElement} className="bg-inherit text-sm text-emerald-200 disabled:text-emerald-400 outline-none grow py-2 px-1" />
+    <textarea id="prompt" value={textValue} disabled={disabled} ref={textAreaRef} rows={MIN_ROWS}
+      autoFocus autoComplete="off" autoCorrect="off"
+      autoCapitalize="off" maxLength={150}
+      onKeyDown={onKeyDownHandler}
+      onInput={e => setTextValue(e.currentTarget.value)}
+      onChange={onChangeResizeElementHandler}
+      className="bg-inherit text-sm text-emerald-200 disabled:text-emerald-400 outline-none grow py-2 px-1" />
   )
 }
 
