50674beb5fcbc3790b6fc1572d1563fda8cfe4c9
Ces-D <58753718+Ces-D@users.noreply.github.com>
2023-09-29 15:59:50 -0400

    [wasm] build pipeline with wasm ready
    - script building wasm binaries for main app resolving

---
+++ packages/wasm/mega-utils/src/lib.rs
@@ -0,0 +1,14 @@
+mod utils;
+mod svg;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, mega-utils!");
+}
---
+++ packages/wasm/mega-utils/src/svg/path/mod.rs
@@ -0,0 +1,3 @@
+mod command;
+
+pub struct Data {}
