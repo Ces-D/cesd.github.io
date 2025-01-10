791f91d6e35d74c33dd5021d029f298d18c34aae
Ces-D <58753718+Ces-D@users.noreply.github.com>
2025-01-10 13:08:04 -0500

    progress: chat message styles

--- www/src/lib/components/ChatMessage.svelte
+++ www/src/lib/components/ChatMessage.svelte
@@ -2,8 +2,19 @@
   import type { Message } from '$lib/types';
 
   const { message, user }: Message = $props();
+  // TODO: Add a way to display the users name or an icon for the bot?
 </script>
 
-<p class={['w-3/4', { 'mr-auto': user === 'user', 'ml-auto': user === 'bot' }]}>
-  {message}
-</p>
+<div
+  class={[
+    'max-w-4/6 rounded-full my-2 px-2 py-1',
+    {
+      'mr-auto bg-primary-300 dark:bg-primary-100': user === 'user',
+      'ml-auto bg-secondary-300 dark:bg-secondary-100': user === 'bot',
+    },
+  ]}
+>
+  <p class={[{ '!text-primary-900': user === 'user', '!text-secondary-900': user === 'bot' }]}>
+    {message}
+  </p>
+</div>
--- www/tailwind.config.cjs
+++ www/tailwind.config.cjs
@@ -1,5 +1,31 @@
 import flowbitePlugin from 'flowbite/plugin';
 
+const PRIMARY_COLOR = {
+  50: '#FFF5F2',
+  100: '#FFF1EE',
+  200: '#FFE4DE',
+  300: '#FFD5CC',
+  400: '#FFBCAD',
+  500: '#FE795D',
+  600: '#EF562F',
+  700: '#EB4F27',
+  800: '#CC4522',
+  900: '#A5371B',
+};
+
+const SECONDARY_COLOR = {
+  50: '#eff6ff',
+  100: '#dbeafe',
+  200: '#bfdbfe',
+  300: '#93c5fd',
+  400: '#60a5fa',
+  500: '#3b82f6',
+  600: '#2563eb',
+  700: '#1d4ed8',
+  800: '#1e40af',
+  900: '#1e3a8a',
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -43,38 +69,40 @@ export default {
         white: '#eeedf9',
         black: '#1b1a1c',
       },
-      primary: {
-        50: '#FFF5F2',
-        100: '#FFF1EE',
-        200: '#FFE4DE',
-        300: '#FFD5CC',
-        400: '#FFBCAD',
-        500: '#FE795D',
-        600: '#EF562F',
-        700: '#EB4F27',
-        800: '#CC4522',
-        900: '#A5371B',
-      },
-      outlineColor: {
-        300: '#FFD5CC',
-      },
+      primary: PRIMARY_COLOR,
+      secondary: SECONDARY_COLOR,
+      outlineColor: { 300: PRIMARY_COLOR[300] },
       ringColor: {
         primary: {
-          300: '#FFD5CC',
-          800: '#CC4522',
+          300: PRIMARY_COLOR[300],
+          800: PRIMARY_COLOR[800],
         },
       },
       textColor: {
         primary: {
-          400: '#FFBCAD',
-          500: '#FE795D',
-          900: '#A5371B',
+          400: PRIMARY_COLOR[400],
+          500: PRIMARY_COLOR[500],
+          900: PRIMARY_COLOR[900],
+        },
+        secondary: {
+          400: SECONDARY_COLOR[400],
+          500: SECONDARY_COLOR[500],
+          900: SECONDARY_COLOR[900],
+        },
+      },
+      backgroundColor: {
+        primary: {
+          100: PRIMARY_COLOR[100],
+          300: PRIMARY_COLOR[300],
+        },
+        secondary: {
+          100: SECONDARY_COLOR[100],
+          300: SECONDARY_COLOR[300],
         },
       },
       borderColor: {
-        primary: {
-          900: '#A5371B',
-        },
+        primary: { 900: PRIMARY_COLOR[900] },
+        secondary: { 900: SECONDARY_COLOR[900] },
       },
     },
   },
